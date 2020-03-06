ea676ef812f39190bb2f098341e1586b90a70bea
rajesh <rajesh.k.khadka@gmail.com>
2020-03-07 00:36:17 +0545

    keep constants

--- auth/manager/auth_manager.py
+++ auth/manager/auth_manager.py
@@ -4,14 +4,17 @@ from auth.models.user import User
 from core.manager.mail_manager import send_email
 from core.manager.token_manager import generate_token, decode_token
 
+EMAIL_VERIFICATION = 'Email Verification'
+EMAIL_KEY = 'email'
+EMAIL_VERIFICATION_HTML = 'email_verification.html'
+
 
 def create_user(user):
     user.save()
-    token = generate_token({'email': user.email})
-    html = render_template('email_verification.html',
-                           url=F'http://localhost.com/email-verification?token={token}')
+    token = generate_token({EMAIL_KEY: user.email})
+    html = render_template(EMAIL_VERIFICATION_HTML, url=F'http://localhost.com/email-verification?token={token}')
     send_email(recipients=[user.email],
-               subject='Email Verification',
+               subject=EMAIL_VERIFICATION,
                body='Reset your password',
                html=html)
     return user
@@ -19,7 +22,7 @@ def create_user(user):
 
 def verify_email(token):
     decoded_token = decode_token(token)
-    user = User.find_by_email(decoded_token['email'])
+    user = User.find_by_email(decoded_token[EMAIL_KEY])
     if user is None: return
     user.email_verified = True
     user.commit()
