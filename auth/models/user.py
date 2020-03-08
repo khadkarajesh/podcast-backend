cf4de383d027a48106b2e60a953b0a4c91df2361
rajesh <rajesh.k.khadka@gmail.com>
2020-03-08 11:01:02 +0545

    drop username from users table

--- auth/models/user.py
+++ auth/models/user.py
@@ -4,7 +4,6 @@ from extentions import db
 
 class User(BaseModel):
     __tablename__ = 'users'
-    username = db.Column(db.String(), nullable=False)
     password = db.Column(db.String(), nullable=False)
     email = db.Column(db.String(), nullable=False)
     email_verified = db.Column(db.Boolean(), default=False)
